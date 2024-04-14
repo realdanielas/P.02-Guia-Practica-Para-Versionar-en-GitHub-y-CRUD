fff6d8a0b4bfdf41e2b02a700110fa7a8dc65c95
realdanielas <daniela9.saavedra@gmail.com>
2024-04-14 17:52:10 -0600

    Add project files.

---
+++ P.02 Guia Practica Para Versionar en GitHub y CRUD/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace P._02_Guia_Practica_Para_Versionar_en_GitHub_y_CRUD
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
